fd8eda1d0bfa607660bdff81daa9117591cef3ad
Misaka <pengqiangqiang@gmail.com>
2024-08-18 18:16:35 +0800

    ``` feat(github-actions): 添加Docker构建和运行步骤
    在GitHub Actions工作流中添加新步骤，以在SSH远程服务器上拉取代码后构建并运行Docker镜像。这将使用新的Dockerfile.txt文件来构建镜像并将其作为后台服务运行。
    
    此更新修改了工作流配置，现在将执行以下操作：
    - 使用SSH连接到远程服务器。
    - 在服务器上的指定目录中拉取最新的代码。
    - 使用Dockerfile.txt构建Docker镜像。
    - 运行构建的Docker镜像作为后台服务。
    
    同时，这个提交还增加了一个`.gitignore`文件，以忽略不必要的虚拟环境文件，从而保持仓库的整洁和高效。
    ```

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #just a test for the github
+#创建一个基于flask的web应用，显示hello world
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+#启动应用
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4001, debug=True)
