798516bded1ca05ee6c49e4a20a36a5131fe1e3b
Misaka <pengqiangqiang@gmail.com>
2024-08-18 19:16:14 +0800

    fix(main): 修正hello_world函数返回值
    修正了hello_world函数，更新了其返回值，以包含额外的问候语"What a brave new world this is!"。
    这将导致在访问根路由时显示更新的消息。
    
    变动参考:
    ```diff
    @@ -4,7 +?,? @@
    @@ -4,? +?,? @@
     def hello_world():
    -    return 'Hello World!'
    +    return 'Hello World! What a brave new world this is!'
     #启动应用
    ```

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! What a brave new world this is!'
 #启动应用
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4001, debug=True)
